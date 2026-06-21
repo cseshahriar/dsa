b926a11aeea657e4a1695ba5798c97f274392d13
cseshahriar <cse.shahriar.hosen@gmail.com>
2026-06-21 10:44:39 +0600

    power of threee

---
+++ dsa-in-python-basics-to-advance/py/pow_of_three.py
@@ -0,0 +1,15 @@
+# check power of two
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        # base case
+        if(n <= 0):
+            return False
+
+        if n == 1:
+            return True
+
+        if n % 3 != 0:
+            return False
+
+        # recursive case
+        return self.isPowerOfThree(n/3)
