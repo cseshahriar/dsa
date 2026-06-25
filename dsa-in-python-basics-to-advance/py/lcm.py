394531d5b8bdb47b0f24ac6ab68866558c425072
cseshahriar <cse.shahriar.hosen@gmail.com>
2026-06-25 19:35:13 +0600

    pow of x pow n

--- dsa-in-python-basics-to-advance/py/lcm.py
+++ dsa-in-python-basics-to-advance/py/lcm.py
@@ -1,3 +1,14 @@
+# https://www.youtube.com/watch?v=0Xh_qqKZHMU
+"""
+num1 = 60 
+num2 = 24 
+60 = 2 x 2 x 3 x 5
+24 = 2 x 2 x 2 x 3
+gcd(common) = 2 x 2 x 3 = 12
+lcm (common, uncommon)= 2 x 2 x 3  x 2 x 5 = 120
+lcm = num1 x num2 / gcd
+"""
+
 def gcd(a, b):
     if b == 0:
         return a
