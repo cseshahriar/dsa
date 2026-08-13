762a84407a5dec8df3214ef206d1e91d9aa2fdd9
cseshahriar <cse.shahriar.hosen@gmail.com>
2026-08-13 10:09:32 +0600

    Binary tree

---
+++ dsa-with-subeen-sir/datastructure/tree/binary_tree.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node Node;
+
+struct node {
+    int data;
+    Node *left;
+    Node *right;
+};
+
+Node *create_node(int item) {
+    Node *new_node = (Node *) malloc(sizeof(Node));
+    if(new_node == NULL) {
+        printf("Error! Could not create a new node\n");
+        exit(1);
+    }
+    new_node->data = item;
+    new_node->left = NULL;
+    new_node->right = NULL;
+    return new_node;
+}
+
+void add_left_child(Node *node, Node *child) {
+    node->left = child;
+}
+
+void add_right_child(Node *node, Node *child) {
+    node->right = child;
+}
+
+Node *create_tree() {
+    Node *two = create_node(2); // root
+    Node *seven = create_node(7);
+    Node *nine = create_node(9);
+    add_left_child(two, seven);
+    add_right_child(two, nine);
+
+    Node *one = create_node(1);
+    Node *six = create_node(6);
+    add_left_child(seven, one);
+    add_right_child(seven, six);
+
+    return two;
+}
+
+int main() {
+
+    Node *root = create_tree();
+    printf("%d\n", root->data);
+    return 0;
+}
---
+++ dsa-with-subeen-sir/datastructure/tree/tree.c
@@ -0,0 +1,7 @@
+// fist node is root, must one root, not more than 1
+// initial left right edge
+// parent, child node
+// leaf node: jar kono child nai
+// internal node: majer node
+// degree: total number of children is degree, left, right, middle nde
+// path: 2 ta node ar modde connect korar nodes, je node ar sahajje jao jay
